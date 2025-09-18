e93718f9f2180dcc5e222198536abc35a5471793
SubparEscobar <aaron.escobar1997@gmail.com>
2025-09-18 16:20:23 -0500

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import 'react-native-gesture-handler';
+import 'react-native-screens/gesture-handler';
 import Main from "./screens/MainComponent";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return <Main />
+  return (
+    <NavigationContainer>
+      <Main /> 
+    </NavigationContainer>
+  );
 }
