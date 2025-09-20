07a3dc4010b9d398ec90e9b5c937d9506cdeb48c
SubparEscobar <aaron.escobar1997@gmail.com>
2025-09-20 11:10:32 -0500

    Added partner information to AboutScreen

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,9 +1,5 @@
-import { useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { PROMOTIONS } from '../shared/promotions';
-import { PARTNERS } from '../shared/partners';
 
 const ContactScreen = () => {
     return (
